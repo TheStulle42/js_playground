e65381b4dc51f7a58ea6925ddfea5dbfe1798250
TheStulle42 <karl.naumann08@web.de>
2024-07-09 11:31:26 +0200

    add secret_message

---
+++ secret_message.js
@@ -0,0 +1,7 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+secretMessage.push('to', 'Program');
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(4, 7, 'know');
+console.log(secretMessage.join());
